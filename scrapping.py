7a80d8c2bd89caabf9c01f1e29034e5691b8052c
aloka-cuelogic <alok.agarwal@cuelogic.co.in>
2015-07-01 18:07:34 +0530

    formatting code

--- scrapping.py
+++ scrapping.py
@@ -3,13 +3,18 @@ import requests
 import time
 
 AREAS = ['Hadapsar', 'Swargate', 'Viman Nagar']
-HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'}
+HEADERS = {'User-Agent':
+           'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'}
 
-#with open('HTML/2.html', 'r') as page:
+# with open('HTML/2.html', 'r') as page:
 
 for area in AREAS:
 
-    url_str = 'http://www.magicbricks.com/property-for-rent/residential-real-estate?proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment,Service-Apartment&Locality=%s&cityName=Pune&BudgetMin=5,000&BudgetMax=10,000' % area
+    print "Searching result in " + area
+
+    url_str = 'http://www.magicbricks.com/property-for-rent/residential-real-estate?' + \
+        'proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,' + \
+        'Studio-Apartment,Service-Apartment&Locality=%s&cityName=Pune&BudgetMin=5,000&BudgetMax=10,000' % area
 
     user_page = requests.get(url_str)
     tree = html.fromstring(user_page.text)
@@ -23,8 +28,6 @@ for area in AREAS:
         print "Search Result: http://www.magicbricks.com" + s[1]
         time.sleep(10)
 
-        
-
         page = requests.get(
             'http://www.magicbricks.com' + s[1], headers=HEADERS)
 
@@ -32,8 +35,10 @@ for area in AREAS:
 
         from_site = treee.xpath('//meta[@property="og:title"]/@content')[0]
         description = treee.xpath('//meta[@name="Description"]/@content')[0]
-        price = treee.xpath('//*[@id="rightAgentH"]/div[2]/div[2]/div[3]/div[2]/ul/li[1]/div/span/text()')[0]
-        property_id = treee.xpath('//*[@id="rightAgentH"]/div[2]/div[2]/div[1]/span/text()')[0]
+        price = treee.xpath(
+            '//*[@id="rightAgentH"]/div[2]/div[2]/div[3]/div[2]/ul/li[1]/div/span/text()')[0]
+        property_id = treee.xpath(
+            '//*[@id="rightAgentH"]/div[2]/div[2]/div[1]/span/text()')[0]
 
         print 'from_site:', from_site
         print 'description:', description
