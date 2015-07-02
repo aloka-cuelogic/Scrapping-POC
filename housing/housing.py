5328cd720d01d57eeb75168dd217b1ccf9bc54f4
it126 <deepali-b@cuelogic>
2015-07-02 18:47:12 +0530

    Worked on review comments Merging to same pull request

--- housing/housing.py
+++ housing/housing.py
@@ -4,27 +4,30 @@ from lxml import html
 import requests
 import time
 
-
+# @TODO:Make list of User-Agent and randomly attach User-Agent for every request
 HEADERS = {'User-Agent':
            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'}
 
 
-def scrap_housing():
-    place = raw_input('Enter city: ').lower()
-    area = raw_input('Enter area (For multiple area enter ',' seprated values): ').lower()
-    AREAS = area.split(',')
-    for area in AREAS:
+def parse_content(city, area):
+    areas = area.split(',')
+    for area in areas:
         page = requests.get(
-            'https://housing.com/in/rent/%s/%s'% (place,area),
+            'https://housing.com/in/rent/%s/%s'% (city, area),
             headers=HEADERS)
 
         tree = html.fromstring(page.text)
 
         product_url_list = tree.xpath('//div[@class="heading-content information"]/a[@href]/@href')
-        print product_url_list
+
         for link in product_url_list:
+            # Crawling hits the website to get all the results
+            # This may lead your IP to be blocked by host company
+            # To prevent this we are making small delay of 10 seconds between two 
+            # consecutive requests
+            # Also setting headers in request to make host feel that a request is coming from browser
             time.sleep(10)
-            page = requests.get(link,headers=HEADERS)
+            page = requests.get(link, headers=HEADERS)
 
             tree = html.fromstring(page.text)
 
@@ -33,6 +36,7 @@ def scrap_housing():
             description = tree.xpath('string(//meta[@name="description"]/@content)').strip()
             price = tree.xpath('string(//div[@class="banner"]/div[1]/div[2]/div[1]/span[2]/text())').strip()
             property_id = tree.xpath('string(//div[@class="apartment-header"]/div[2]/ul/li[4]/text())').strip()
+            # @deepalib  This is for testing purpose and need to be removed once entire app is developed
             print '\n\n'
             print 'from_site:', from_site
             print 'product_url:', product_url
@@ -42,4 +46,13 @@ def scrap_housing():
             print '---------------------------------------------------'
 
 if __name__ == '__main__':
-    scrap_housing()
+    '''
+    Getting place and area is temporary 
+    it will be removed once actual crawler is up
+    as crawler will be taking care of it
+    '''
+
+    place = raw_input("Enter city: ").lower()
+    area = raw_input("Enter area (For multiple area enter ','" \
+                     "seprated values): ").lower()
+    parse_content(place, area)
