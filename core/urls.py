cb49684eb7176763c29efc366ed9221174f062bd
Ganesh Palkar <ganesh.palkar@cuelogic.co.in>
2015-07-06 14:33:57 +0530

    removed unwanted apps

--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^get_results/$', 'core.views.get_results',
-        name='get_results'),
-)
--- scrapper/databases/views.py
+++
@@ -1,9 +0,0 @@
-from databases.sulekha import sulekha
-
-
-def index(request):
-    place = raw_input("Enter city: ").lower()
-    area = raw_input(
-        "Enter area (For multiple area enter ,seprated values): ").lower()
-
-    sulekha.parse_content(place, area)
