5c6a323f1b21c478037c07a6ca20bd531a81eff0
ganeshp-cuelogic <ganesh.palkar@cuelogic.co.in>
2015-09-15 19:31:59 +0530

    removing print statements

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -11,31 +11,23 @@ AREAS = ["Hadapsar", "Warje", "Viman Nagar", "Wakad"]
 
 def housing_officials():
     """Scrape for a list of current officeholders using an officials API"""
-    print "I am inside housing_officials"
-    print "===================="
     housing.parse_content(CITY, AREAS)
     return True
 
 
 def magicbricks_officials():
     """Scrape for a list of current officeholders using an officials API"""
-    print "I am inside magicbricks_officials"
-    print "===================="
     magicBricks.parse_content(CITY, AREAS)
     return True
 
 
 def propertywala_officials():
     """Scrape for a list of current officeholders using an officials API"""
-    print "I am inside propertywala_officials"
-    print "===================="
     propertywala.parse_content(CITY, AREAS)
     return True
 
 
 def sulekha_officials():
     """Scrape for a list of current officeholders using an officials API"""
-    print "I am inside sulekha_officials"
-    print "===================="
     sulekha.parse_content(CITY, AREAS)
     return True
